8c296e82366ad863cda92bc39bba6303208105cf
JmaesGeoghegan <73890808+JmaesGeoghegan@users.noreply.github.com>
2020-12-01 17:27:20 +0000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <string>
-#include "Prompt.h"
+//#include "Prompt.h"
 #include "Manager.h"
 
 using namespace std;
@@ -18,7 +18,7 @@ int main()
 
     string username, password;
     string input;
-    cout << "login, skip login or register:" << endl;
+    /*cout << "login, skip login or register:" << endl;
     do{
         cout << ">>";
         cin >> input;
@@ -44,7 +44,7 @@ int main()
     //in the future we will implement login support but for now the login is only cosmetic.
     //this *needs to be changed, James*
 
-
+*/
 
     cout << "Welcome to Gym Manager (Name & Patent Pending)!\n\nPlease enter a command to continue with your gym activity management.\n";
 
@@ -59,8 +59,7 @@ int main()
         cout << "workout: A calculator for the calories you should burn based on your workout routine. (This is still WIP)\n";
         cout << "bmi: A Body Mass Index tool to determine your fitness.\nend: Turn the thing off.\n";
         cout << "login: Starts the login process.\n";
-        cout << "register: Starts the register process to allow you to log in.\n";
-        cout << "stats: shows you information about you that you have inputted. (Requires being logged in).\n";}
+        cout << "register: Starts the register process to allow you to log in.\n";}
 
         //login while you are logged out.
         else if(prompt == "login")
@@ -79,15 +78,24 @@ int main()
         //The BMI Calculator. This is where you will use your inputted weight and height to determine your health.
         else if(prompt == "bmi")
                {
-                        float height = 0;
-                        float weight = 0;
-                        cout << "\nWelcome to the BMI Calculator.\n\nUse this tool to determine your fitness and personal health based on your height and weight.\n";
-                        cout << "Please enter your Height in meters: ";
-                        cin >> height;
-                        cout << "Now please enter your Weight in Kilograms: ";
-                        cin >> weight;
-                        Prompt dumby(height,weight);
-                        dumby.body();
+                    float height = 0;
+                    float weight = 0;
+                    cout << "\nWelcome to the BMI Calculator.\n\nUse this tool to determine your fitness and personal health based on your height and weight.\n";
+                    cout << "Please enter your Height in meters: ";
+                    cin >> height;
+                    cout << "Now please enter your Weight in Kilograms: ";
+                    cin >> weight;
+                    int bmi = weight/(height*height);
+                    cout << "Your Body mass index value is " << bmi << endl;
+                    {if(bmi>18&&bmi<25){cout << "Your body is a healthy weight. Congratulations!\n\n";}
+                    else if(bmi>24&&bmi<30){cout << "You are a little overweight, but not too much. Reducing your weight a little will improve your health!\n\n";}
+                    else if(bmi>29&&bmi<40){cout << "You are obese and need to lose some weight. Reducing your weight will improve your health.\n\n";}
+                    else if(bmi>99){cout << "Holy shit. Bruh. You fuckin Heavy.\n\n";}
+                    else if(bmi>39){cout << "You are extremely obese and your health is at risk. Reducing your weight substantially will greatly improve your health\n\n";}
+                    else if(bmi<19){cout << "You are underweight. Putting on some weight will improve your health.\n\n";}}
+
+
+                    //export this info into a new file so that we can save data in the future
                }
 
         //This is the 'Calorie Burning (Workout) Calculator(tm)'. This will work out the estimated calories burnt in a session of exercise. It will require your weight to function.
