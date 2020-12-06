416b0d9f318f139817a71be8bc9b259800075ec8
C19432284 <73892387+C19432284@users.noreply.github.com>
2020-12-06 18:45:46 +0000

    Update profile.cpp

--- profile.cpp
+++ profile.cpp
@@ -39,11 +39,12 @@ void profile::registration(){
     }
     }while(profile1.GetloggedIn() == 0);
 
-    cout << "1. BMI calculater - Calculates your bmi." << endl;
-    cout << "2. Workout routine tracker - Tracks your workout." << endl;
+    cout << "1. BMI calculater - Calculates your body/mass index." << endl;
+    cout << "2. Workout routine tracker - Tracks your workout and the calaories burnt." << endl;
     cout << "3. Profile Statistics - Overview of profile details." << endl;
     cout << "4. Main menu - Go back to login and registration." << endl;
-    cout << "0. END" << endl;
+    cout << "5. Help menu - List of commands." << endl;
+    cout << "6. END" << endl;
 }
 //Login function that authenticates the user and also loads user data into the class.
 void profile::login(){
@@ -126,6 +127,7 @@ void profile::logonT(){
     totalV++;
     profile1.SettotalV(totalV);
 }
+// resets profile class.
 void profile::reset(){
     profile1.Setusername(" ");
     profile1.Setpassword(" ");
@@ -134,6 +136,128 @@ void profile::reset(){
     profile1.Setbmi(0);
     profile1.SettotalCals(0);
     profile1.SettotalV(0);
+}
+// Workout tracker to findout how much calories burnt.
+void profile::workout()
+{
+    float mins = 0;
+    string exercise = "";
+    float exer = 0;
+    float weet = 0;
+    int y = 1;
+    starh:
+    float l = 0;
+    while(y == 1) {
+
+        if (profile1.Getweight() >= 56 && profile1.Getweight() <= 111)
+         cout << "\nWhat exercise did you do?\n" << " \n A: Weight Lifting: general \n B: Aerobics: water \n C: Stretching, Hatha Yoga \n D: Calisthenics: moderate \n E: Riders: general \n F: Aerobics: low impact \n G: Stair Step Machine: general \n H: Weight Lifting: vigorous \n I: Bicycling, Stationary: moderate \n J: Rowing, Stationary: moderate\n K: Aerobics: high impact\n L: Rowing, Stationary: vigorous\n M: Ski Machine: general\n " << endl;
+        else if (profile1.Getweight() < 56 && profile1.Getweight() > 0){
+            cout << "Your weight appears to be lower than the minimum requirements (56KG). \nPlease consult a specialist before performing any strenuous exercises.\nClosing program.";
+            exit(0);}
+        else if (profile1.Getweight() < 0){
+            cout << "Your weight cannot less than 0. \n";
+            goto starh;}
+            else if (profile1.Getweight() > 111){
+            cout << "Your weight appears to be higher than the maximum requirements (111KG). \nPlease consult a specialist before performing any strenuous exercises.\nClosing program." ;
+            exit(0);}
+        else
+{
+
+            cout << "Error.\nInvalid Input.\n";
+            cin.clear();
+            cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            goto starh;
+}           stark:
+            cin >> exercise;
+
+float exer;
+if (exercise == "a" || exercise == "A")
+    exer = 3;
+else if (exercise == "b" || exercise == "B")
+    exer = 4;
+else if (exercise == "c" || exercise == "C")
+    exer = 4;
+else if (exercise == "d" || exercise == "D")
+    exer = 4.5;
+else if (exercise == "e" || exercise == "E")
+    exer = 5;
+else if (exercise == "f" || exercise == "F")
+    exer = 5.5;
+else if (exercise == "g" || exercise == "G")
+    exer = 6;
+else if (exercise == "h" || exercise == "H")
+    exer = 6;
+else if (exercise == "i" || exercise == "I")
+    exer = 7;
+else if (exercise == "j" || exercise == "J")
+    exer = 7;
+else if (exercise == "k" || exercise == "K")
+    exer = 7;
+else if (exercise == "l" || exercise =="L")
+    exer = 8.5;
+else if (exercise == "m" || exercise =="M")
+    exer = 9.5;
+else{
+    cout << "Error invalid input\n";
+    cin.clear();
+    cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    goto stark;}
+
+starp:
+        cout << "\nHow long (in minutes) did you exercise?\n"<<endl;
+    cin >> mins;
+    if (mins > 960){
+        cout << "\nError \nA max of 960 minutes (16 hours) can be achieved daily.\nThis is not recommended.";
+         y = 0;}
+    else if (mins > 240)
+        cout << "\nYou have exercised for an extended period of time. \nIt is recommended you take a break. \nPlease ensure you are well hydrated.\n";
+
+    else if (mins > 0)
+         cout << "\n";
+           else if (mins < 0){
+        cout << "Error.\nInvalid Input.";
+        goto starp;}
+
+            else {
+        cout << "Error.\nInvalid Input.";
+        goto starp;}
+
+float weet;
+
+   if (profile1.Getweight() >= 56 && profile1.Getweight() < 70)
+        weet = 1;
+
+        else if (profile1.Getweight() >= 70 && profile1.Getweight() < 83)
+        weet = 1.24;
+
+        else if (profile1.Getweight() >= 83 && profile1.Getweight() < 97)
+                 weet = 1.48;
+        else if (profile1.Getweight() >= 97 && profile1.Getweight() <= 111)
+                weet = 1.72;
+
+
+    float x;
+    x = weet * mins * exer ;
+
+    cout << "\nYou have burnt " << x << " calories.\n";
+
+    l = l + x;
+    cout << "\nYou burnt " << l << " calories in total.\n";
+    profile1.SettotalCals(l);
+    profile1.save();
+    string z;
+    cout << "\nContinue? Y/N\n" <<endl;
+    cin >> z;
+
+    if (z == "y" || z == "Y")
+    {
+        y = 1;
+    }
+    else if (z == "n" || z == "N")
+    {
+        y = 0;
+    }
+}
 }
 profile::~profile()
 {
