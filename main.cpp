1a6f131c0bdb029491308215483c7d47cb06d2ae
C19432284 <73892387+C19432284@users.noreply.github.com>
2020-12-06 16:02:40 +0000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,36 +2,38 @@
 
 using namespace std;
 
-//global variables
-bool notloggedin = 1;
-
 int main()
 {
-    //First menu to login and register. 
+    //First menu to login and register.
     int prompt, input;
     string username, password;
     profile profile1;
-    cout << "Welcome to Gym Manager \n Please enter a command to continue with your gym activity management.\n" << endl;
+    start:
+    cout << "Welcome to Gym Manager \nPlease enter a command to continue with your gym activity management.\n" << endl;
+    start1:
     do{
-    cout << "1. Login" << endl;
+    cout << "\n1. Login" << endl;
     cout << "2. Register" << endl;
+    cout << ">> ";
     cin >> input;
     if(input == 1){
         profile1.login();
-        notloggedin = 0;
-        profile1.logonT();
-        break;
+        cout << profile1.GetloggedIn() << endl; // testing boolean
     }else if(input == 2){
         profile1.registering();
         break;
     }else{
         cout << "Invalid input. Please type '1' to Login, '2' to Register." << endl;
+        cin.clear();
+        cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        goto start1;
     }
-    }while(notloggedin = 0);
+    }while(profile1.GetloggedIn() == 1);
 
     cout << "1. BMI calculater - Calculates your bmi." << endl;
     cout << "2. Workout routine tracker - Tracks your workout." << endl;
     cout << "3. Profile Statistics - Overview of profile details." << endl;
+    cout << "4. Main menu - Go back to login and registration." << endl;
     cout << "0. END" << endl;
 
     do{
@@ -56,18 +58,25 @@ int main()
 }
         break;
     case 4:{
+            profile1.save();
+            profile1.reset();
+            goto start;
+}
+            break;
+    case 5:{
             cout << "1. BMI calculater - Calculates your bmi." << endl;
             cout << "2. Workout routine tracker - Tracks your workout." << endl;
             cout << "3. Profile Statistics - Overview of profile details." << endl;
+            cout << "4. Main menu - Go back to login and registration." << endl;
             cout << "0. END" << endl;
 }
             break;
     default:
-        cout << "Invalid command. Type '4' to open up the Help menu for a list of commands. " << endl;
+        cout << "Invalid command. Type '5' to open up the Help menu for a list of commands. " << endl;
 }
     }while(prompt != 0);
-
     profile1.save();
 
     return 0;
 }
+
